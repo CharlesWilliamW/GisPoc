bdb04f152209fa3a0cda08d3abb8933b95d7145c
CharlieWu <chiaweicharliewu@gmail.com>
2026-05-21 18:03:13 +0800

    Move current buttons and checkboxes to bottom right

--- GisWinFormsNet8App/GisWinFormsNet8App/SidebarController.cs
+++ GisWinFormsNet8App/GisWinFormsNet8App/SidebarController.cs
@@ -36,14 +36,10 @@ namespace GisWinFormsNet8App
             _splitContainer.Panel1.Padding = new Padding(15, 15, 15, 0);
             _splitContainer.Panel2.BackColor = Color.White;
 
-            _splitContainer.Panel1.Controls.Add(chkBufferMode);
-            _splitContainer.Panel1.Controls.Add(btnClearMeasure);
-            _splitContainer.Panel1.Controls.Add(chkMeasureMode);
-            _splitContainer.Panel1.Controls.Add(btnToggleDisaster);
             _splitContainer.Panel2.Controls.Add(mapControl);
-
             form.Controls.Add(_splitContainer);
 
+            // 設定各控制項樣式
             btnToggleDisaster.Dock = DockStyle.Top;
             btnToggleDisaster.Height = 45;
             btnToggleDisaster.Font = new Font("Microsoft JhengHei", 10, FontStyle.Bold);
@@ -63,6 +59,23 @@ namespace GisWinFormsNet8App
             chkBufferMode.ForeColor = Color.White;
             chkBufferMode.Font = new Font("Microsoft JhengHei", 10);
             chkBufferMode.Padding = new Padding(0, 10, 0, 0);
+
+            // 包在底部 Panel，讓所有控制項貼齊側邊欄左下角
+            var controlPanel = new Panel
+            {
+                Dock = DockStyle.Bottom,
+                Height = 165,
+                Padding = new Padding(0, 0, 0, 10),
+                BackColor = Color.Transparent
+            };
+
+            // Dock=Top 時，最後加入的控制項排在最上方，依序往下疊
+            controlPanel.Controls.Add(chkBufferMode);
+            controlPanel.Controls.Add(btnClearMeasure);
+            controlPanel.Controls.Add(chkMeasureMode);
+            controlPanel.Controls.Add(btnToggleDisaster);
+
+            _splitContainer.Panel1.Controls.Add(controlPanel);
 
             mapControl.Dock = DockStyle.Fill;
 
